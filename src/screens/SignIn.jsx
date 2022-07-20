2695996c8016e35f2112d7a1867eea404bbacd18
brulibra <brulibra03@hotmail.com>
2022-07-20 19:34:20 -0300

    Projeto Ignite Lab RocketHelp - Aula 1

---
+++ src/screens/SignIn.jsx
@@ -0,0 +1,69 @@
+import { useState } from "react"
+import { VStack, Heading, Icon, useTheme } from "native-base"
+import { Envelope, Key } from "phosphor-react-native"
+
+import Logo from "../assets/logo_primary.svg"
+import { Button } from "../components/Button"
+import { Input } from "../components/Input"
+
+export function SignIn() {
+	const [name, setName] = useState("")
+	const [password, setPassword] = useState("")
+	const { colors } = useTheme()
+
+	function handleSignIn(){
+		console.log(name, password);
+	}
+
+	return (
+
+		<VStack
+			flex={1}
+			alignItems="center"
+			bg="gray.600" px={8}
+			pt={24}
+		>
+
+			<Logo />
+
+			<Heading
+				color="gray.100"
+				fontSize="xl"
+				mt={20}
+				mb={6}
+			>
+				Acesse sua conta {name}
+			</Heading>
+
+			<Input
+				mb={4}
+				placeholder="E-mail"
+				onChangeText={setName}
+				InputLeftElement={
+					<Icon
+					as={<Envelope color={colors.gray[300]} />}
+					ml={4}
+					/>}
+			/>
+
+			<Input
+				mb={8}
+				placeholder="Senha"
+				onChangeText={setPassword}
+				InputLeftElement={
+					<Icon
+						as={<Key color={colors.gray[300]} />}
+						ml={4}
+					/>}
+					secureTextEntry
+			/>
+
+			<Button
+				title="Entrar"
+				w="full"
+				onPress={handleSignIn}
+			/>
+
+		</VStack>
+	)
+}
